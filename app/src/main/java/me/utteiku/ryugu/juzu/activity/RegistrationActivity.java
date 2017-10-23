3226e452d55d479b9952711fd092a0ee9ba372e7
miyagi.ryota <kuryugujo9240@gmail.com>
2017-10-23 15:52:50 +0900

    TEST

--- app/src/main/java/me/utteiku/ryugu/juzu/activity/RegistrationActivity.java
+++ app/src/main/java/me/utteiku/ryugu/juzu/activity/RegistrationActivity.java
@@ -1,14 +1,21 @@
 package me.utteiku.ryugu.juzu.activity;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import me.utteiku.ryugu.juzu.DBProvider;
 import me.utteiku.ryugu.juzu.R;
 
 /**
@@ -16,7 +23,6 @@ import me.utteiku.ryugu.juzu.R;
  */
 
 public class RegistrationActivity extends Activity {
-
 
     private static String EXTRA_ITEM_ID = "extra_item_id";
     public static Intent createIntent(Context context, int itemId) {
@@ -30,27 +36,63 @@ public class RegistrationActivity extends Activity {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_registration);
 
-        //Todo from DB
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_sex);
+        final EditText nameEditer = (EditText) findViewById(R.id.name_editer);
+        final DatePicker birtydayPicker = (DatePicker) findViewById(R.id.birthdayPicker);
+        final Spinner sexSpinner = (Spinner) findViewById(R.id.sex_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.sex_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        sexSpinner.setAdapter(adapter);
 
         final Button button = (Button)findViewById(R.id.signUpButton);
         button.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 // クリック時の処理
-                startActivity(EditIconActivity.createIntent(getApplicationContext()));
+                try {
+                    writeDB(nameEditer.getText().toString());
+                    writeDB(birtydayPicker.toString());
+                    writeDB(sexSpinner.toString());
+                } catch (Exception e) {
+                    toast("" + e);
+                }
+                //startActivity(EditIconActivity.createIntent(getApplicationContext()));
             }
         });
 
 
     }
 
+    //DBprovider
+    private void writeDB(String info) throws Exception{
+        // コンテンツプロバイダが提供するデータベースが示すURI
+        Uri uri = Uri.parse("content://me.utteiku.ryugu.juzu.dbprovider/");
 
+        //コンテンツプロバイダが提供するデータベースへのアクセス
+        ContentValues values = new ContentValues();
+        values.put("id", "0");
+        values.put("info",info);
+        int num = getContentResolver().update(uri, values, null, null);
+        if ( num == 0 ) getContentResolver().insert(uri, values);
+    }
 
+    private String readDB() throws Exception{
+        // コンテンツプロバイダが提供するデータベースが示すURI
+        Uri uri = Uri.parse("content://me.utteiku.ryugu.juzu.dbprovider/");
+
+        //コンテンツプロバイダが提供するデータベースへのアクセス
+        Cursor c = this.getContentResolver().query(uri, new String[]{"id", "info"}, "id='0'", null, null);
+        if (c.getCount() == 0) throw new Exception();
+        c.moveToFirst();
+        String str = c.getString(1);
+        c.close();
+        return str;
+    }
+
+    //toast
+    private void toast(String text){
+        if(text.isEmpty()) text = "";
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
 
 }
